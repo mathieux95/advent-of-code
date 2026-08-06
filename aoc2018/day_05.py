487b755f5c8b8ef0997eda94b75cf2d62a10e29d
Matej Gradoš <matej.grados@resistant.ai>
2026-08-06 09:03:49 +0200

    Add solution for day05 + tests

---
+++ aoc2018/day_05.py
@@ -0,0 +1,27 @@
+def read_input(filename: str) -> list[str]:
+    with open(filename) as file:
+        return [line.strip() for line in file]
+
+
+def part1(data: list[str]) -> int:
+    polymer = data[0]
+    stack = []
+
+    for unit in polymer:
+        if stack and unit.swapcase() == stack[-1]:
+            stack.pop()
+        else:
+            stack.append(unit)
+
+    return len(stack)
+
+
+def part2(data: list[str]) -> int:
+    polymer = data[0]
+    best_length = len(polymer)
+
+    for unit in set(polymer.lower()):
+        new_polymer = "".join(c for c in polymer if c.lower() != unit)
+        best_length = min(best_length, part1([new_polymer]))
+
+    return best_length
