5d7e7be11a270ccd8f21a2245472b8ffc166bf27
Matej Gradoš <matej.grados@resistant.ai>
2026-08-16 12:02:51 +0200

    Improve clarity

--- aoc2018/day_04.py
+++ aoc2018/day_04.py
@@ -12,35 +12,37 @@ def parse_log(log: str) -> tuple[int, int, int, int, int, str]:
 
 
 def part1(logs: list[str]) -> int:
-    guard_sleep = {}
-    total_sleep = {}
-    current_guard = None
-    sleep_start = None
+    guard_sleep = parse_guard_sleep(logs)
 
-    for log in sorted(logs):
-        *_, minute, message = parse_log(log)
-
-        if "Guard" in message:
-            current_guard = int(message.split()[1][1:])
-            guard_sleep.setdefault(current_guard, [0] * 60)
-            total_sleep.setdefault(current_guard, 0)
-
-        elif message == "falls asleep":
-            sleep_start = minute
-
-        else:
-            total_sleep[current_guard] += minute - sleep_start
-            for m in range(sleep_start, minute):
-                guard_sleep[current_guard][m] += 1
+    total_sleep = {
+        guard: sum(minutes)
+        for guard, minutes in guard_sleep.items()
+    }
 
     sleepiest_guard = max(total_sleep, key=total_sleep.get)
-    minutes = guard_sleep[sleepiest_guard]
-    sleepiest_minute = minutes.index(max(minutes))
+    sleepiest_minute = guard_sleep[sleepiest_guard].index(
+        max(guard_sleep[sleepiest_guard])
+    )
 
     return sleepiest_guard * sleepiest_minute
 
 
 def part2(logs: list[str]) -> int:
+    guard_sleep = parse_guard_sleep(logs)
+
+    sleepiest_guard, sleepiest_minute = max(
+        (
+            (guard, minute)
+            for guard, minutes in guard_sleep.items()
+            for minute in range(60)
+        ),
+        key=lambda x: guard_sleep[x[0]][x[1]],
+    )
+
+    return sleepiest_guard * sleepiest_minute
+
+
+def parse_guard_sleep(logs: list[str]) -> dict[int, list[int]]:
     guard_sleep = {}
     current_guard = None
     sleep_start = None
@@ -55,19 +57,8 @@ def part2(logs: list[str]) -> int:
         elif message == "falls asleep":
             sleep_start = minute
 
-        else:  # wakes up
+        else:
             for m in range(sleep_start, minute):
                 guard_sleep[current_guard][m] += 1
 
-    max_count = 0
-    sleepiest_guard = None
-    sleepiest_minute = None
-
-    for guard, minutes in guard_sleep.items():
-        for minute, count in enumerate(minutes):
-            if count > max_count:
-                max_count = count
-                sleepiest_guard = guard
-                sleepiest_minute = minute
-
-    return sleepiest_guard * sleepiest_minute
+    return guard_sleep
