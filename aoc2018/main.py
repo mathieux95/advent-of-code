dd014c43389ec23ed4764fb520ad47cba24b1356
Matej Gradoš <61123302+mathieux95@users.noreply.github.com>
2026-09-04 14:57:51 +0200

    Add solution for day12 + test (#13)

--- aoc2018/main.py
+++ aoc2018/main.py
@@ -1,7 +1,7 @@
 import argparse
 from pathlib import Path
 
-from aoc2018 import day_01, day_02, day_03, day_04, day_05, day_06, day_07, day_08, day_09, day_10, day_11
+from aoc2018 import day_01, day_02, day_03, day_04, day_05, day_06, day_07, day_08, day_09, day_10, day_11, day_12
 
 DAYS = {
     1: day_01,
@@ -15,6 +15,7 @@ DAYS = {
     9: day_09,
     10: day_10,
     11: day_11,
+    12: day_12,
 
 }
 
